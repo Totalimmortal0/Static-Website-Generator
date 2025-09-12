2fc46577ed8d40f29dd515210e0d8671b6590048
Matt <Trivial6991@proton.me>
2025-09-12 20:04:57 -0300

    additional fixes

--- src/copy_source_to_destination.py
+++ src/copy_source_to_destination.py
@@ -1,13 +1,6 @@
 import os
 import shutil
 
-# check if destination directory exists, if it does cleans it, if not creates it
-if os.path.exists("public"):
-    shutil.rmtree("public")
-os.mkdir("public")
-dest_dir_path = "public"
-
-source_dir_path = "static"
 
 def copy_files(source_dir_path, dest_dir_path):
     if not os.path.exists(dest_dir_path):
